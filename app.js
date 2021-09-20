4706e2415341792ffe0a835ef7aee96b14d38f90
ILYASSE <ilyasse@pc-dt-ilyasse>
2021-09-20 16:36:26 +0200

    commit # 02: change message; supress the name of charcter; calcule the number of the day; log a message depending of the day number

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 
 // #region My test region
 const message = "Hello world ! ";
-const characterName = "Toto";
+const dayOfWeek = new Date().getDate();
+if (dayOfWeek !== 0 || dayOfWeek !== 7) {
+    console.log("It's a working day.");
+} else {
+    console.log("Today is a weekend!");
+};
 
-console.log(message + characterName);
-// #endregion
+// console.log(message + characterName);
+// // #endregion
